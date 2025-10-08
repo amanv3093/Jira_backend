a3725684831da52ae35f27b5a1f2fa37fee72ad3
Aman verma <amanv3093@gmail.com>
2025-10-08 18:13:34 +0530

    ✨ feat(task): implement task filtering and assignment
    - add filtering by status, priority, project, and assignee
    - enable searching tasks by name
    - enhance task creation with member record validation

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -22,6 +22,18 @@ class TaskController {
           res.status(401).json({ error: "User not authenticated" });
           return;
         }
+        console.log("assignments", assignments);
+        const memberRecords = await prisma.member.findMany({
+          where: {
+            userId: { in: assignments.map((a) => a.userId) },
+            // projectId,
+          },
+        });
+
+        if (memberRecords.length === 0) {
+          res.status(401).json({ error: "Member not found" });
+          return;
+        }
 
         const task = await prisma.task.create({
           data: {
@@ -33,15 +45,8 @@ class TaskController {
             projectId,
           },
         });
-        console.log("assignments", assignments);
-        const memberRecords = await prisma.member.findMany({
-          where: {
-            userId: { in: assignments.map((a) => a.userId) },
-            projectId,
-          },
-        });
-        console.log("memberRecords", memberRecords);
 
+        console.log("memberRecords", memberRecords);
         if (memberRecords.length > 0) {
           for (const m of memberRecords) {
             await prisma.taskAssignment.create({
@@ -83,24 +88,53 @@ class TaskController {
       }
       try {
         const id = req.params.id;
-        console.log("Enter", id);
+        // console.log("Enter", id);
         if (!id) {
           res.status(401).json({ error: "workspaceId  are required" });
           return;
         }
-        const tasks = await prisma.task.findMany({
-          where: {
-            project: {
-              workspaceId: id as string,
-            },
-            assignments: {
-              some: {
-                member: {
-                  userId: user?.id as string,
-                },
-              },
-            },
+
+        const { status, priority, project, assignee, search } = req.query;
+        const statusArray = typeof status === "string" ? status.split(",") : [];
+        const priorityArray =
+          typeof priority === "string" ? priority.split(",") : [];
+        const projectArray =
+          typeof project === "string" ? project.split(",") : [];
+        const assigneeArray =
+          typeof assignee === "string" ? assignee.split(",") : [];
+
+        const whereClause: any = {
+          project: {
+            workspaceId: id as string,
           },
+        };
+        if (statusArray.length > 0) {
+          whereClause.status = { in: statusArray };
+        }
+
+        if (priorityArray.length > 0) {
+          whereClause.priority = { in: priorityArray };
+        }
+
+        if (projectArray.length > 0) {
+          whereClause.projectId = { in: projectArray };
+        }
+
+        if (assigneeArray.length > 0) {
+          whereClause.assignments = {
+            some: {
+              memberId: { in: assigneeArray },
+            },
+          };
+        }
+
+        if (search && typeof search === "string" && search.trim() !== "") {
+          whereClause.OR = [
+            { task_name: { contains: search, mode: "insensitive" } },
+          ];
+        }
+        const tasks = await prisma.task.findMany({
+          where: whereClause,
           include: {
             project: true,
             createdBy: true,
@@ -115,7 +149,13 @@ class TaskController {
             },
           },
         });
-
+        // assignments: {
+        //   some: {
+        //     member: {
+        //       userId: user?.id as string,
+        //     },
+        //   },
+        // },
         if (!tasks) {
           res.status(404).json({ error: "Task not found" });
           return;
